cd58780292c582c03eb500abcd35e6c9295c1440
Excellence-daniel <43383067+Excellence-daniel@users.noreply.github.com>
2018-09-18 17:50:03 +0100

    Add files via upload

---
+++ zackdaniels/index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<title> TESTS </title>
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<!--Font Awesome CSS-->
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+    <script src="js/jquery.min.js"></script>	
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="css/style.css">
+    
+    <script src="js/jquery-1.11.3.min.js"></script>	
+		<script src="js/bootstrap.min.js"></script>
+    
+    <script src="js/jquery-1.8.3.min.js"></script>
+    
+    
+    <style>
+        #pdd{
+            padding-left:0px; 
+            padding-right:0px;
+        }
+        
+        
+        #boxy{
+    background: transparent;
+    border-top: transparent;
+    border-left: transparent;
+    border-right: transparent;
+    border-bottom-color: transparent;
+    border-radius: 0px;
+    box-shadow: none;
+        }
+        
+       .fa-twitter {
+  display: inline-block;
+  border-radius: 60px;
+  box-shadow: 0px 0px 2px #888;
+  padding: 0.5em 0.6em;
+background:#2cbfea;
+    color:white;
+
+}
+    a:hover{
+            text-decoration: none;
+        }
+        
+     .fa-facebook-f {
+  display: inline-block;
+  border-radius: 60px;
+  box-shadow: 0px 0px 2px #888;
+  padding: 0.5em 0.6em;
+background:#4562b5;
+    color:white;
+         width:40px; 
+         text-align:center;
+
+}
+  
+   
+     .fa-google {
+  display: inline-block;
+  border-radius: 60px;
+  box-shadow: 0px 0px 2px #888;
+  padding: 0.5em 0.6em;
+background:white;
+    color:red;
+
+}
+        
+@media screen and (max-width: 768px) {
+        }
+        
+    
+    </style>
+    </head>
+<body style="background:#f8fafb;">
+    
+    <div class="container-fluid">
+        <div class ="col-md-12">
+            <div class ="col-md-12">
+                <div class ="col-md-2 col-xs-12"> <img src ="img/img/Ghostlamp.PNG"></div>
+                <div class ="col-md-10"></div>                    
+            </div>
+            
+            <div class="col-md-12" style="margin-top:5%;">
+            <div class="col-md-8"><img src="img/img/Ghostlamp-picture-1.PNG" class="img-responsive"></div>
+                <div class="col-md-4">
+                <h2><p> Welcome Back :) </p></h2>
+                
+                <h5 style="line-height:1.7;">To keep connected with us please login with your personal information by email address and password <i class="fas fa-bell" style="color:#fd8e00;"></i></h5>
+                <br>
+<div class="col-md-12" style="padding-left:0px;background:#e8dfdf; padding-right:0px; border-radius:10px;">
+    
+    <div class="col-md-12" id="pdd" style="margin-top:2%;">
+        
+        <div class="col-md-2" style="margin-top:4%;"><i class="fas fa-envelope" style="font-size: 35px;color:#9a98986e;"></i></div>
+        
+        
+    <div class="col-md-8" id="pdd">
+        <div class="col-md-12" id="pdd" style="font-size:12px; color:#aba4a4;"> Email Address</div>
+<div class ="col-md-12" id="pdd" style="margin-top:2%;"><input type="email" class="form-control" id="boxy" placeholder=""></div>
+    </div>
+        
+        
+<div class="col-md-2" style="margin-top:7%;"><i class="fas fa-check-circle" style="font-size: 16px;color:#5ab35a;"></i></div>
+    </div>
+    
+    
+    <!--password-->
+    <div class="col-md-12" id="pdd" style="margin-top:4%; background:white;border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;">
+<div class="col-md-2" style="margin-top:4%;"><i class="fas fa-lock" style="font-size: 37px;color:#9a98986e;"></i></div>
+        
+        
+<div class="col-md-8" id="pdd" style="margin-top:3%;">
+    <div class="col-md-12" id="pdd" style="font-size:12px; color:#aba4a4;"> Password</div>
+<div class ="col-md-12" id="pdd" style="margin-top:2%;"><input type="password" class="form-control" id="boxy" placeholder=""></div>
+    </div>
+        
+        
+<div class="col-md-2" style="margin-top:6%;"></div>
+<div class="col-md-12" style="margin-top:2%;"></div>
+    </div>
+    <!--/password-->
+    
+    
+    
+    
+   </div>
+                    
+<div class="col-md-12" style="margin-top:5%;font-size:15px;">    
+<div class="col-md-6 col-xs-6" style="text-align:left;">
+<span style="margin-top:3%;"><i class="fas fa-check-circle" style="font-size: 20px;color:#5ab35a;"></i></span> <span>Remember Me</span>
+    </div>
+<div class="col-md-6 col-xs-6" style="text-align:right;"><a href="#">Forget password?</a></div>
+                </div>                             
+                    
+                                
+<div class="col-md-12" style="margin-top:7%;" id="#pdd">
+                    
+    <div class="col-md-5 col-xs-6" id="pdd"><button class="btn btn-primary btn-md" style="border-radius:50px; color:white; width:130px;">Login Now </button> </div>
+                    
+    <div class="col-md-5 col-xs-6" id="pdd" style="">
+<button class="btn btn-primary btn-md" style="border-radius:50px;background:white;border-color:white;color:black; width:130px;">Create Account </button> 
+    </div>
+                    
+    <div class="col-md-2"></div>
+                </div>                   
+
+<div class="col-md-12" style="margin-top:10%;">
+        <p>Or you can join with:</p>
+    <br>
+   <span style="font-size:20px;">
+<a href=""><i class="fab fa-google" style="margin-right:3%"></i></a>
+       
+<a href=""><i class="fab fa-facebook-f" style="margin-right:3%;"></i></a>
+       
+    <a href=""><i class="fab fa-twitter"></i></a>
+    
+    </span> 
+                    
+                    </div>
+                
+                </div>
+            
+            
+            </div>
+            
+        </div>
+        
+        </div>
+    
+    </body>
+</html>
